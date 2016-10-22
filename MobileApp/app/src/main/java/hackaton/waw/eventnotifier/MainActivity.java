0259da0f908163512de16fceb143338edd64308b
Tomasz Garbus <tomasz.garbus1@gmail.com>
2016-10-22 12:39:07 +0200

    Call toolbar programatically

--- MobileApp/app/src/main/java/hackaton/waw/eventnotifier/MainActivity.java
+++ MobileApp/app/src/main/java/hackaton/waw/eventnotifier/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
@@ -56,15 +58,15 @@ public class MainActivity extends AppCompatActivity
         eventManager = new EventManager();
 
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         getFragmentManager().beginTransaction().replace(R.id.content_main, MainFragment.newInstance()).commit();
 
